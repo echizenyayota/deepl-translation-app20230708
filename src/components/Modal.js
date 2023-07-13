4eac1a877cfa13a15c3d0118bf2ad3b7d44f197a
echizenyayota <virtualmisso30@gmail.com>
2023-07-13 18:45:02 +0900

    add the langauges in the Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,39 @@
 const Modal = () => {
   return (
-    <div>
-      Modal
+    <div className="option-list">
+      <div className="search-bar">
+        <select className="source-language">
+          <option value="BG">Bulgarian</option>
+          <option value="CS">Czech</option>
+          <option value="DA">Danish</option>
+          <option value="DE">German</option>
+          <option value="EL">Greek</option>
+          <option value="EN">English</option>
+          <option value="ES">Spanish</option>
+          <option value="ET">Estonian</option>
+          <option value="FI">Finnish</option>
+          <option value="FR">French</option>
+          <option value="HU">Hungarian</option>
+          <option value="ID">Indonesian</option>
+          <option value="IT">Italian</option>
+          <option value="JA">Japanese</option>
+          <option value="KO">Korean</option>
+          <option value="LT">Lithuanian</option>
+          <option value="LV">Latvian</option>
+          <option value="NB">Norwegian Bokmål</option>
+          <option value="NL">Dutch</option>
+          <option value="PL">Polish</option>
+          <option value="PT">Portuguese</option>
+          <option value="RO">Romanian</option>
+          <option value="RU">Russian</option>
+          <option value="SK">Slovak</option>
+          <option value="SL">Slovenian</option>
+          <option value="SV">Swedish</option>
+          <option value="TR">Turkish</option>
+          <option value="UK">Ukrainian</option>
+          <option value="ZH">Chinese</option>
+        </select>
+      </div>  
     </div>
   )
 }
